0ffe33fffe7cb150e18b1747bb53cd67823eb791
uniquemozilla22 <uniqgaming26@gmail.com>
2021-02-22 13:21:41 +0545

    Removed Bloatware from master

--- app/src/main/java/com/example/labexercise/MainActivity.java
+++ app/src/main/java/com/example/labexercise/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String let ="hey this is me Lab1";
 
     }
 }
