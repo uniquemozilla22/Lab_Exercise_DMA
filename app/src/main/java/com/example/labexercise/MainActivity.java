40b824913391c241bad8e1f14749f3d88c1a1bc9
uniquemozilla22 <uniqgaming26@gmail.com>
2021-02-22 13:20:23 +0545

    Branch Checkout

--- app/src/main/java/com/example/labexercise/MainActivity.java
+++ app/src/main/java/com/example/labexercise/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String let ="This is the LabExecrise part";
     }
 }
